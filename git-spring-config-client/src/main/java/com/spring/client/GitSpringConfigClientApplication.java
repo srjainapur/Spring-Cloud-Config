d45b25608b58088b789320b6caa1eba389d4286d
k307121 <sjainapur@zensar.com>
2019-03-19 15:42:47 +0530

    spring-cloud-config

---
+++ git-spring-config-client/src/main/java/com/spring/client/GitSpringConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.spring.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringConfigClientApplication.class, args);
+	}
+
+}
---
+++ git-spring-config-client/src/main/java/com/spring/client/controller/MessageRestController.java
@@ -0,0 +1,18 @@
+package com.spring.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageRestController {
+	@Value("${message:Hello world - Config Server is not working..pelase check}")
+    private String msg;
+ 
+    @RequestMapping("/msg")
+    String getMsg() {
+        return this.msg;
+    }
+}
